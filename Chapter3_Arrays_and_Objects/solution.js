66d6d172d8479f3139caf05bcdc28918a1c1c8aa
Atinka Laryea <devatinka@gmail.com>
2025-11-10 20:11:50 +0000

    Finished first 4 exercises

---
+++ Chapter3_Arrays_and_Objects/solution.js
@@ -0,0 +1,25 @@
+// Task 1
+const arr1 = ["Joe", "Horse", "Hack", 3, 4, 2];
+const firstItem = arr1[0];
+const secondItem = arr1[arr1.length - 1];
+
+console.log(firstItem);
+console.log(secondItem);
+
+// Task 2
+const arr2 = ["Mango", "Beans", "Salad"]
+let added = arr2.push("Banana");
+let removed = arr2.shift();
+
+console.log(arr2);
+
+// Task 3
+const arr3 = ["Mike", "Samoa", 22];
+const includes = arr3.includes("Mike");
+console.log(includes);
+
+
+// Task 4
+const arr4 = [1, 3, 5, 7, 11];
+const index = arr4.indexOf(11);
+console.log(index);
